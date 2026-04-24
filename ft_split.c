943239ea77a73f80d1b868d0a28359a8ff443378
nunoelbling <nunoelbling26@gmail.com>
2026-04-24 20:04:57 +0100

    24.04.26

--- ft_split.c
+++ ft_split.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nuelblin <nuelblin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nuno1tap <nuno1tap@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 15:20:53 by nuelblin          #+#    #+#             */
-/*   Updated: 2026/04/23 16:08:38 by nuelblin         ###   ########.fr       */
+/*   Updated: 2026/04/24 19:43:42 by nuno1tap         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	count_words(const char *s, char c)
+static	int	count_words(const char *s, char c)
 {
 	size_t	count;
 	size_t	i;
@@ -27,7 +27,7 @@ int	count_words(const char *s, char c)
 	return (count);
 }
 
-char	**ft_free(char **res, int i)
+static	char	**ft_free(char **res, int i)
 {
 	while (i--)
 		free(res[i]);
@@ -38,7 +38,7 @@ char	**ft_free(char **res, int i)
 char	**ft_split(char const *s, char c)
 {
 	char	**res;
-	char	*next_sep;
+	char	*sep;
 	int		i;
 
 	if (!s)
@@ -51,22 +51,20 @@ char	**ft_split(char const *s, char c)
 	{
 		while (*s && *s == c)
 			s++;
-		if (*s)
-		{
-			next_sep = ft_strchr(s, c);
-			if (!next_sep)
-				next_sep = (char *)s + ft_strlen(s);
-			res[i++] = ft_substr(s, 0, next_sep - s);
-			if (!res[i - 1])
-				return (ft_free(res, i - 1));
-			s = next_sep;
-		}
+		if (!*s)
+			break ;
+		sep = ft_strchr(s, c);
+		if (!sep)
+			sep = (char *)s + ft_strlen(s);
+		res[i++] = ft_substr(s, 0, (size_t)(sep - s));
+		if (!res[i - 1])
+			return (ft_free(res, i - 1));
+		s = sep;
 	}
-	res[i] = NULL;
-	return (res);
+	return (res[i] = NULL, res);
 }
 
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main(void)
 {
@@ -75,7 +73,7 @@ int main(void)
 
     i = 0;
 
-    res = ft_split("alalalallalalalalalalallalalallalalalalalald,svlksna faf ga ", 'a');
+    res = ft_split("alalalallalalallalald,svlksna faf ga ", 'a');
 
     if (!res)
         return (1);
@@ -88,4 +86,4 @@ int main(void)
     free(res);
     
     return (0);
-}
+} */
